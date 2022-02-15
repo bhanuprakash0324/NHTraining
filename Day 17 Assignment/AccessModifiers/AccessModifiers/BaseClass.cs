fee408f849e43fc57c80575b071f1f54c9c325f0
bhanuprakash0324 <bhanuprakash0324@gmail.com>
2022-02-15 20:55:31 +0530

    Day17 Assignment

---
+++ Day 17 Assignment/AccessModifiers/AccessModifiers/BaseClass.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccessModifiers
+{
+    //Author: Bhanu Prakash Reddy
+    //create two assemblies with 3 classes in first assembly, 2 classes in other assembly
+    public class BaseClass
+    {
+        public int a;
+        private int b;
+        protected int c;
+        internal int d;
+        protected internal int e;
+
+        public void BaseClassMethod()
+        {
+            //All variables are accessed within the same base class
+
+            a = 11;
+            b = 22;
+            c = 33;
+            d = 44;
+            e = 55;
+        }
+    }
+
+    public class DerivedClass : BaseClass
+    {
+        public void DerivedClassMethod()
+        {
+            //In the direct derived class, it does not access private variable in the same assembly
+
+            a = 11;
+            //b = 22; 
+            c = 33;
+            d = 44;
+            e = 55;
+
+        }
+    }
+
+    public class OtherClass
+    {
+        public void OtherClassMethod()
+        {
+            BaseClass bc = new BaseClass();
+
+            //Private and Protected variables cannot be accessed in the other class method in same assembly 
+            bc.a = 11;
+            //bc.b = 22;
+            //bc.c = 33;
+            bc.d = 44;
+            bc.e = 55;
+
+        }
+    }
+}
---
+++ Day 17 Assignment/AccessModifiers/PublicLibrary/PublicDerivedClass.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AccessModifiers;
+
+namespace PublicLibrary
+{
+    public class PublicDerivedClass : BaseClass
+    {
+        public void PublicDerivedClassMethod()
+        {
+            //Private and internal variables cannot be accessed in public derived class method in other assembly
+            
+            a = 11;
+            //b = 22;
+            c = 33;
+            //d = 44;
+            e = 55;
+        }
+    }
+
+    public class PublicOtherClass
+    {
+        public void PublicOtherClassMethod()
+        {
+            BaseClass bc = new BaseClass();
+
+            // Except public variable, we cannot access other variables in other class method in other assembly
+
+            bc.a = 11;
+            //bc.b = 22;
+            //bc.c = 33;
+            //bc.d = 44;
+            //bc.e = 55;
+        }
+    }
+}
