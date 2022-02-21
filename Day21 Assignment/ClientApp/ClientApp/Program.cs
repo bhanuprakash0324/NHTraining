81bdf457df95c25a5642de7ef2a66744f9fd2394
bhanuprakash0324 <bhanuprakash0324@gmail.com>
2022-02-21 11:56:41 +0530

    Day21 Assignment

---
+++ Day21 Assignment/ClientApp/ClientApp/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClientApp.ServiceReference1;
+
+namespace ClientApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            WebService1SoapClient obj=new WebService1SoapClient();
+
+            Console.WriteLine($"The factorial of the number is {obj.Factorial(4)}");
+            Console.WriteLine($"Sum of the numbers is {obj.Add(4,6)}");
+            Console.WriteLine($"Multiplication of the numbers is {obj.Mul(7,3)}");
+            Console.WriteLine($"Divison of the numbers is {obj.Div(30,10)}");
+
+            Console.ReadLine();
+        }
+    }
+}
